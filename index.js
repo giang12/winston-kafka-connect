9ce5494d0b7af8b1372803125d9fb8b12a06370e
Giang Nguyen <nggiang12@gmail.com>
2017-09-28 13:04:49 -0700

    init

---
+++ index.js
@@ -0,0 +1,83 @@
+const util = require('util'),
+	winston = require('winston'),
+	_ = require('lodash'),
+	Kafka = require('kafka-node'),
+	KafkaClient = Kafka.KafkaClient,
+	HighLevelProducer = Kafka.HighLevelProducer;
+
+function WinstonKafkaLogger(options) {
+	//
+	// Name this logger
+	//
+	this.name = options.name || "WinstonKafkaLogger";
+
+	this.timestamp = options.timestamp || function() {
+		return new Date()
+	};
+
+	this.formatter = options.formatter || JSON.stringify;
+
+	//
+	// Set the level from your options
+	//
+	this.level = options.level || "info";
+
+	this.meta = options.meta || {}; //{host-name: os.hostname()}
+	//
+	// Configure your storage backing as you see fit
+	//
+	this.connected = false;
+	this.kafkaHost = options.kafkaHost || "localhost:9092";
+	this.clientId = options.clientId || "winston-kafka-logger";
+	this.topic = options.topic || "winston-logs";
+	//this.sslOptions = options.sslOptions
+	this.client = new KafkaClient({
+		kafkaHost: this.kafkaHost,
+		clientId: this.clientId
+			//sslOptions: this.sslOptions
+	});
+	this.producer = new HighLevelProducer(this.client);
+	this.producer
+		.on('ready', () => {
+			this.connected = true
+		})
+		.on('error', err => {
+			this.connected = false;
+			console.log(err);
+			throw new Error("winston-kafka-logger cannot connect to Kafka server");
+		})
+};
+
+//
+// Inherit from `winston.Transport` so you can take advantage
+// of the base functionality and `.handleExceptions()`.
+//
+util.inherits(WinstonKafkaLogger, winston.Transport);
+
+WinstonKafkaLogger.prototype.log = function(level, msg, meta, callback) {
+	//
+	// Store this message and metadata, maybe use some custom logic
+	// then callback indicating success.
+	//
+	if (this.connected) {
+		const payload = {
+			level: level,
+			msg: msg,
+			meta: _.defaultsDeep({}, meta, this.meta),
+			timestamp: this.timestamp()
+		};
+		const payloads = [{
+			topic: this.topic,
+			messages: [this.formatter(payload)]
+		}];
+		try {
+			this.producer.send(payloads, () => { /**nop**/ });
+		} catch (err) {
+			console.log(err);
+		}
+	}
+	callback(null, true);
+};
+
+
+module.exports = WinstonKafkaLogger;
